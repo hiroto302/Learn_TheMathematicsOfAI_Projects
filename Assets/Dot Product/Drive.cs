662e1d1dedf006f6175d6367015cffeb2c633c38
hiroto302 <i.am.a.hiroto@gmail.com>
2021-06-04 18:52:26 +0900

    Update Drive.cs

--- Assets/Dot Product/Drive.cs
+++ Assets/Dot Product/Drive.cs
@@ -18,37 +18,63 @@ public class Drive : MonoBehaviour {
     void CalculateAngle() {
 
         // Tanks foward facing vector
-        Vector3 tankForwardVector = this.transform.up;
+        Vector3 tF = this.transform.up;
         // Vector to the fuel
-        Vector3 fuelVector = fuel.transform.position - this.transform.position;
+        Vector3 fD = fuel.transform.position - this.transform.position;
 
         // Calculate the dot product
-        float dot = tankForwardVector.x * fuelVector.x + tankForwardVector.y * fuelVector.y;
-        float angle = Mathf.Acos(dot / (tankForwardVector.magnitude * fuelVector.magnitude));
+        float dot = tF.x * fD.x + tF.y * fD.y;
+        float angle = Mathf.Acos(dot / (tF.magnitude * fD.magnitude));
 
         // Output the angle to the console
         Debug.Log("Angle: " + angle * Mathf.Rad2Deg);
         // Output Unitys angle
-        Debug.Log("Unity Angle: " + Vector3.Angle(tankForwardVector, fuelVector));
+        Debug.Log("Unity Angle: " + Vector3.Angle(tF, fD));
 
         // Draw a ray showing the tanks forward facing vector
-        Debug.DrawRay(this.transform.position, tankForwardVector * 10.0f, Color.green, 2.0f);
+        Debug.DrawRay(this.transform.position, tF * 10.0f, Color.green, 2.0f);
         // Draw a ray showing the vector to the fuel
-        Debug.DrawRay(this.transform.position, fuelVector, Color.red, 2.0f);
+        Debug.DrawRay(this.transform.position, fD, Color.red, 2.0f);
+
+        int clockwise = 1;
+
+        // Check the z value of the crossproduct and negate the direction if less than 0
+        if (Cross(tF, fD).z < 0.0f)
+            clockwise = -1;
+
+        // Use Unity to work out the angle for you
+        float unityAngle = Vector3.SignedAngle(tF, fD, this.transform.forward);
+
+        // Get the tank to face the fuel
+        this.transform.Rotate(0.0f, 0.0f, unityAngle);
+    }
+
+    // Calculate the Cross Product
+    Vector3 Cross(Vector3 v, Vector3 w) {
+
+        float xMult = v.y * w.z - v.z * w.y;
+        float yMult = v.z * w.x - v.x * w.z;
+        float zMult = v.x * w.y - v.y * w.x;
+
+        Vector3 crossProd = new Vector3(xMult, yMult, zMult);
+        return crossProd;
     }
 
     // Calculate the distance from the tank to the fuel
     void CalculateDistance() {
 
         // Tank position
-        Vector3 tankPosition = this.transform.position;
+        Vector3 tP = this.transform.position;
         // Fuel position
-        Vector3 fuelPosition = fuel.transform.position;
+        Vector3 fP = fuel.transform.position;
 
         // Calculate the distance using pythagoras
-        float distance = Mathf.Sqrt(Mathf.Pow(tankPosition.x - fuelPosition.x, 2.0f) + Mathf.Pow(tankPosition.y - fuelPosition.y, 2.0f) + Mathf.Pow(tankPosition.z - fuelPosition.z, 2.0f));
+        float distance = Mathf.Sqrt(Mathf.Pow(tP.x - fP.x, 2.0f) +
+                         Mathf.Pow(tP.y - fP.y, 2.0f) +
+                         Mathf.Pow(tP.z - fP.z, 2.0f));
+
         // Calculate the distance using Unitys vector distance function
-        float unityDistance = Vector3.Distance(tankPosition, fuelPosition);
+        float unityDistance = Vector3.Distance(tP, fP);
 
         // Print out the two results to the console
         Debug.Log("Distance: " + distance);
